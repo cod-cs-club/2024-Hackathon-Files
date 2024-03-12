d627ab921bd45e8e3000305740e3f087f5edf6d5
Voy7 <wkulak3@gmail.com>
2024-03-11 20:34:29 -0500

    Updated templates

--- NodeJS-Template.js
+++ NodeJS-Template.js
@@ -1,5 +1,6 @@
 import { Client, GatewayIntentBits } from 'discord.js'
 
+// Initialize bot with it's intents/permissions
 const bot = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -10,15 +11,18 @@ const bot = new Client({
   ]
 })
 
+// Triggered when bot is ready
 bot.on('ready', () => {
-  console.log('Bot is ready!')
+  console.log(`Bot logged in as: ${bot.user.tag}`)
 })
 
+// Triggered when anyone sends a message
 bot.on('messageCreate', async (message) => {
   if (message.author.bot) return // If sender is a bot, ignore it
 
+  // If message starts with 'hello', bot will respond
   if (message.content.startsWith('hello')) {
-    message.reply('Hello there!')
+    message.channel.send(`Hello ${message.author.username}! I am a basic Discord bot.`)
   }
 })
 
--- Python-Template.py
+++ Python-Template.py
@@ -1,30 +1,31 @@
 import discord
 
-# Define your intents
+# Define bot's intents/permissions
 intents = discord.Intents.default()
+intents.guilds = True
+intents.messages = True
+intents.message_content = True
+intents.reactions = True
+intents.members = True
 
 # Initialize the bot client with intents
 client = discord.Client(intents=intents)
 
-# Channel ID where the bot will respond
-channel_id = 1199907582307999795 #change to whatever channel ID you're on
-
+# Triggered when bot is ready
 @client.event
 async def on_ready():
-    # Find the channel object by ID
-    channel = client.get_channel(channel_id)
-    if channel:
-        # Send a message to the channel
-        await channel.send("Bot is ready!")
+    print(f'Bot logged in as: {client.user}')
 
-    print(f'We have logged in as {client.user}')
-
+# Triggered when anyone sends a message
 @client.event
 async def on_message(message):
-    # Check if the message is in the specified channel and not sent by the bot itself
-    if message.channel.id == channel_id and message.author != client.user:
-        # Respond to the message
+    # If sender is bot, ignore it
+    if message.author == client.user:
+        return
+    
+    # If message starts with 'hello', bot will respond
+    if message.content.startswith('hello'):
         await message.channel.send(f'Hello {message.author.mention}! I am a basic Discord bot.')
 
 # Run the bot with your Discord bot token
-client.run('MTIwMDE2MDE3MTk2MDMxNTkyNA.GdkNY5.vBeQWUlJaaCDb17c6OJNdt-RVF36FpBWY3n3uY')
+client.run('BOT_TOKEN_HERE')
